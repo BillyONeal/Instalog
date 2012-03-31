16abf8ec14da4932e3623ec8a3e632bb705e081d
Jacob Snyder <jrs213@case.edu>
2012-03-30 22:23:49 -0400

    Implementations moved to cpp

--- LogCommon/EventLog.hpp
+++ LogCommon/EventLog.hpp
@@ -20,35 +20,12 @@ namespace Instalog { namespace SystemFacades {
 		std::wstring strings; // TODO
 		std::wstring dataString;
 
-		EventLogEntry(PEVENTLOGRECORD pRecord)
-			: timeGenerated(pRecord->TimeGenerated)
-			, timeWritten(pRecord->TimeWritten)
-			, eventId(pRecord->EventID & 0x0000FFFF)
-			, eventType(pRecord->EventType)
-			, eventCategory(eventCategory)
-			, sourceName(reinterpret_cast<const wchar_t*>(reinterpret_cast<char*>(pRecord) + sizeof(*pRecord)))
-			, computerName(reinterpret_cast<const wchar_t*>(reinterpret_cast<char*>(pRecord) + sizeof(*pRecord) + sourceName.size() * sizeof(wchar_t) + sizeof(wchar_t)))
-			, strings(reinterpret_cast<const wchar_t*>(reinterpret_cast<char*>(pRecord) + pRecord->StringOffset))
-			, dataString(reinterpret_cast<const wchar_t*>(reinterpret_cast<char*>(pRecord) + pRecord->DataOffset)) 
-		{
-		
-		}
+		EventLogEntry(PEVENTLOGRECORD pRecord);
 
 		/// @brief	Move constructor.
 		///
 		/// @param [in,out]	e	The EventLogEntry &amp;&amp; to process.
-		EventLogEntry(EventLogEntry && e)
-			: timeGenerated(e.timeGenerated)
-			, timeWritten(e.timeWritten)
-			, eventId(e.eventId)
-			, eventType(e.eventType)
-			, eventCategory(eventCategory)
-			, sourceName(std::move(e.sourceName))
-			, computerName(std::move(e.computerName))
-			, dataString(std::move(e.dataString))
-		{
-		
-		}
+		EventLogEntry(EventLogEntry && e);
 	};
 
 	/// @brief	Wrapper around Win32 event log
@@ -64,56 +41,7 @@ namespace Instalog { namespace SystemFacades {
 		/// @brief	Destructor, frees the handle
 		~EventLog();
 
-		std::vector<EventLogEntry> ReadEvents()
-		{
-			std::vector<EventLogEntry> eventLogEntries;
-			eventLogEntries.reserve(16 * 1024 /* approximate based on dev machine */);
-
-			DWORD lastError = ERROR_SUCCESS;
-			std::vector<char> buffer(0x7ffff /* (max size of buffer) */); // http://msdn.microsoft.com/en-us/library/aa363674.aspx
-			DWORD bytesRead;
-			DWORD minNumberOfBytesNeeded;
-
-			while (lastError == ERROR_SUCCESS)
-			{
-				BOOL status = ::ReadEventLogW(handle, 
-					EVENTLOG_SEQUENTIAL_READ | EVENTLOG_FORWARDS_READ, 
-					0,
-					buffer.data(),
-					static_cast<DWORD>(buffer.size()),
-					&bytesRead,
-					&minNumberOfBytesNeeded);
-
-				if (status == 0)
-				{
-					lastError = ::GetLastError();
-
-					if (lastError == ERROR_INSUFFICIENT_BUFFER)
-					{
-						lastError = ERROR_SUCCESS;
-
-						buffer.resize(minNumberOfBytesNeeded);
-					}
-					else if (lastError != ERROR_HANDLE_EOF)
-					{
-						Win32Exception::Throw(lastError);
-					}
-				}
-				else
-				{
-					PEVENTLOGRECORD pRecord = reinterpret_cast<PEVENTLOGRECORD>(buffer.data());
-
-					while (reinterpret_cast<char*>(pRecord) < buffer.data() + bytesRead)
-					{
-						eventLogEntries.emplace_back(EventLogEntry(pRecord));
-
-						pRecord = reinterpret_cast<PEVENTLOGRECORD>(reinterpret_cast<char*>(pRecord) + pRecord->Length);
-					}
-				}
-			}
-
-			return eventLogEntries;
-		}
+		std::vector<EventLogEntry> ReadEvents();
 	};
 
 }}
