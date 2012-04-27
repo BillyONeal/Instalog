87a7efc0c3e03d92e973a1509960a42f21e874a2
Billy O'Neal <billy.oneal@gmail.com>
2012-04-27 19:04:57 -0400

    Removed unneeded boost build script.

--- boostbuild.bat
+++
@@ -1,4 +0,0 @@
-call bootstrap.bat
-b2 -j4 --with-locale address-model=32 runtime-link=static link=static --stagedir=stage32
-b2 -j4 --with-locale address-model=64 runtime-link=static link=static --stagedir=stage64
-del /f /s /q bin.v2
