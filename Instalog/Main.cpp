44ad8082391e64b91cc18ca5d49e89c2a596657e
Billy O'Neal <billy.oneal@gmail.com>
2012-10-30 19:19:22 -0700

    Nasty hack to print C++ exception information and stack trace with StackWalker.

--- Instalog/Main.cpp
+++ Instalog/Main.cpp
@@ -7,6 +7,16 @@
 #include <fcntl.h>
 #define NOMINMAX
 #include <windows.h>
+
+// HACK HACK HACK: In order to print the stack trace, we need to catch the exception as a SEH
+// exception; before MSVC++ has done the translation back into a C++ exception for us.
+// Include VC's own headers in order to decode that exception inside a SEH handler.
+#pragma warning(push)
+#pragma warning(disable: 4201) // Nonstandard extension: nameless struct/union
+#define _VC_VER_INC
+#include "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\crt\src\ehdata.h"
+#pragma warning(pop)
+#include "ThirdParty/StackWalker/StackWalker.h"
 #include "LogCommon/Wow64.hpp"
 #include "LogCommon/Win32Exception.hpp"
 #include "LogCommon/UserInterface.hpp"
@@ -34,6 +44,78 @@ struct ConsoleInterface : public Instalog::IUserInterface
 
 using namespace Instalog;
 
+class InstalogStackWalker : public StackWalker, private boost::noncopyable
+{
+    bool insideInstalogCode;
+public:
+    InstalogStackWalker()
+        : insideInstalogCode(false)
+    {
+    }
+    virtual void OnLoadModule(LPCSTR, LPCSTR mod, DWORD64, DWORD, DWORD result, LPCSTR symType, LPCSTR pdbName, ULONGLONG)
+    {
+        // Only print messages about loaded symbols when the symbols are loaded from a PDB file (e.g. as Instalog's are)
+        if (result == ERROR_SUCCESS && strcmp(symType, "PDB") == 0)
+        {
+            std::cerr << "Loaded symbols for " << mod << " (from " << pdbName << ")" << std::endl;
+        }
+    }
+    virtual void OnCallstackEntry(CallstackEntryType, CallstackEntry &entry) override
+    {
+        std::string name(entry.undFullName);
+        if (name == "_CxxThrowException")
+        {
+            // _CxxThrowException will be one past the top of the exception stack in Instalog's source
+            insideInstalogCode = true;
+            return;
+        }
+        else if (name == "__tmainCRTStartup")
+        {
+            // __tmainCRTStartup is the first function visible in the CRT; it is the function that calls
+            // main().
+            insideInstalogCode = false;
+            return;
+        }
+        
+        if (insideInstalogCode)
+        {
+            std::cerr << entry.lineFileName << " (" << entry.lineNumber << "): " << entry.undFullName << std::endl;
+        }
+    }
+};
+
+LONG WINAPI UnhandledExceptionHandler(LPEXCEPTION_POINTERS pointers)
+{
+    if (pointers->ExceptionRecord->ExceptionCode != EH_EXCEPTION_NUMBER)
+    {
+        return EXCEPTION_CONTINUE_SEARCH;
+    }
+
+    std::cerr << "FAILURE!\n";
+    auto ehExceptionRecord = reinterpret_cast<EHExceptionRecord *>(pointers->ExceptionRecord);
+    auto ehObjectPtr = ehExceptionRecord->params.pExceptionObject;
+    auto stdException = static_cast<std::exception*>(ehObjectPtr);
+    auto hresultExt = dynamic_cast<Instalog::SystemFacades::HresultException*>(stdException);
+    auto win32Ext = dynamic_cast<Instalog::SystemFacades::Win32Exception*>(stdException);
+	if (hresultExt != nullptr)
+	{
+		std::wcerr << L"HRESULT Error: 0x" << std::hex << hresultExt->GetErrorCode() << L": " << hresultExt->GetErrorStringW() << std::endl;
+	}
+    else if (win32Ext != nullptr)
+	{
+		std::wcerr << L"Win32 Error: 0x" << std::hex << win32Ext->GetErrorCode() << L": " << win32Ext->GetWideMessage() << std::endl;
+	}
+    else
+	{
+		std::cerr << "C++ Exception: " << stdException->what() << std::endl;
+	}
+    std::cerr << "Call Stack:\n";
+    InstalogStackWalker walker;
+    walker.ShowCallstack(::GetCurrentThread(), pointers->ContextRecord);
+    ::TerminateProcess(::GetCurrentProcess(), 1);
+    return 0; // compiler warnings....
+}
+
 /// @brief	Main entry-point for this application.
 int main()
 {
@@ -45,50 +127,29 @@ int main()
         L"|___|_| |_|___/\\__\\__,_|_|\\___/ \\__, |\n"
         L"by Jacob Snyder and Billy ONeal |___/\n";
 
+    ::SetUnhandledExceptionFilter(UnhandledExceptionHandler);
 	Instalog::SystemFacades::Com com;
-#ifdef NDEBUG
-	try
-	{
-#endif
-		if (Instalog::SystemFacades::IsWow64())
-		{
-			std::cerr << "This program is not designed to be run under WOW64 mode. Please download the x64 copy of Instalog instead.";
-			return -1;
-		}
-		std::wofstream outFile(L"Instalog.txt", std::ios::trunc | std::ios::out);
-		ScriptParser sd;
-		sd.AddSectionDefinition(std::unique_ptr<ISectionDefinition>(new RunningProcesses));
-		sd.AddSectionDefinition(std::unique_ptr<ISectionDefinition>(new PseudoHjt));
-		sd.AddSectionDefinition(std::unique_ptr<ISectionDefinition>(new ServicesDrivers));
-		sd.AddSectionDefinition(std::unique_ptr<ISectionDefinition>(new EventViewer));
-		sd.AddSectionDefinition(std::unique_ptr<ISectionDefinition>(new MachineSpecifications));
-		sd.AddSectionDefinition(std::unique_ptr<ISectionDefinition>(new RestorePoints));
-		sd.AddSectionDefinition(std::unique_ptr<ISectionDefinition>(new InstalledPrograms));
-		sd.AddSectionDefinition(std::unique_ptr<ISectionDefinition>(new FindStarM));
-		wchar_t const defaultScript[] =
-			L":RunningProcesses\n:PseudoHijackThis\n:ServicesDrivers\n:FindStarM\n:EventViewer\n:MachineSpecifications\n:RestorePoints\n:InstalledPrograms\n";
-		Script s = sd.Parse(defaultScript);
-		std::unique_ptr<IUserInterface> ui(new ConsoleInterface);
-		s.Run(outFile, ui.get());
-        outFile.close();
-#ifdef NDEBUG
-	}
-	catch (Instalog::SystemFacades::HresultException const& ex)
-	{
-		std::wcerr << L"Hresult Error: 0x" << std::hex << ex.GetErrorCode() << L": " << ex.GetErrorStringW() << std::endl;
-	}
-	catch (Instalog::SystemFacades::Win32Exception const& ex)
-	{
-		std::wcerr << L"Win32 Error: 0x" << std::hex << ex.GetErrorCode() << L": " << ex.GetWideMessage() << std::endl;
-	}
-	catch (std::exception const& ex)
-	{
-		std::cerr << "Exception: " << ex.what() << std::endl;
-	}
-    std::wcout << L"Press any key to close this window.";
+    if (Instalog::SystemFacades::IsWow64())
+    {
+        std::cerr << "This program is not designed to be run under WOW64 mode. Please download the x64 copy of Instalog instead.";
+        return -1;
+    }
+    std::wofstream outFile(L"Instalog.txt", std::ios::trunc | std::ios::out);
+    ScriptParser sd;
+    sd.AddSectionDefinition(std::unique_ptr<ISectionDefinition>(new RunningProcesses));
+    sd.AddSectionDefinition(std::unique_ptr<ISectionDefinition>(new PseudoHjt));
+    sd.AddSectionDefinition(std::unique_ptr<ISectionDefinition>(new ServicesDrivers));
+    sd.AddSectionDefinition(std::unique_ptr<ISectionDefinition>(new EventViewer));
+    sd.AddSectionDefinition(std::unique_ptr<ISectionDefinition>(new MachineSpecifications));
+    sd.AddSectionDefinition(std::unique_ptr<ISectionDefinition>(new RestorePoints));
+    sd.AddSectionDefinition(std::unique_ptr<ISectionDefinition>(new InstalledPrograms));
+    sd.AddSectionDefinition(std::unique_ptr<ISectionDefinition>(new FindStarM));
+    wchar_t const defaultScript[] =
+        L":RunningProcesses\n:PseudoHijackThis\n:ServicesDrivers\n:FindStarM\n:EventViewer\n:MachineSpecifications\n:RestorePoints\n:InstalledPrograms\n";
+    Script s = sd.Parse(defaultScript);
+    std::unique_ptr<IUserInterface> ui(new ConsoleInterface);
+    s.Run(outFile, ui.get());
+    outFile.close();
+    std::wcout << L"Press enter to close this window.";
     std::wcin.get();
-#else
-	system("notepad.exe Instalog.txt");
-#endif
-
 }
