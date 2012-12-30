04c9202ac403414904d911d7361fec42a3e2a93c
Billy O'Neal <billy.oneal@gmail.com>
2012-12-30 03:06:33 -0800

    Reverted stack walker changes. Made sure everything builds with XP toolset.

--- Instalog/Main.cpp
+++ Instalog/Main.cpp
@@ -8,15 +8,6 @@
 #define NOMINMAX
 #include <windows.h>
 
-// HACK HACK HACK: In order to print the stack trace, we need to catch the exception as a SEH
-// exception; before MSVC++ has done the translation back into a C++ exception for us.
-// Include VC's own headers in order to decode that exception inside a SEH handler.
-#pragma warning(push)
-#pragma warning(disable: 4201) // Nonstandard extension: nameless struct/union
-#define _VC_VER_INC
-#include "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\crt\src\ehdata.h"
-#pragma warning(pop)
-#include "ThirdParty/StackWalker/StackWalker.h"
 #include "LogCommon/Wow64.hpp"
 #include "LogCommon/Win32Exception.hpp"
 #include "LogCommon/UserInterface.hpp"
@@ -44,82 +35,41 @@ struct ConsoleInterface : public Instalog::IUserInterface
 
 using namespace Instalog;
 
-class InstalogStackWalker : public StackWalker, private boost::noncopyable
-{
-    bool insideInstalogCode;
-public:
-    InstalogStackWalker()
-        : insideInstalogCode(false)
-    {
-    }
-    virtual void OnLoadModule(LPCSTR, LPCSTR mod, DWORD64, DWORD, DWORD result, LPCSTR symType, LPCSTR pdbName, ULONGLONG)
-    {
-        // Only print messages about loaded symbols when the symbols are loaded from a PDB file (e.g. as Instalog's are)
-        if (result == ERROR_SUCCESS && strcmp(symType, "PDB") == 0)
-        {
-            std::cerr << "Loaded symbols for " << mod << " (from " << pdbName << ")" << std::endl;
-        }
-    }
-    virtual void OnCallstackEntry(CallstackEntryType, CallstackEntry &entry) override
-    {
-        std::string name(entry.undFullName);
-        if (name == "_CxxThrowException")
-        {
-            // _CxxThrowException will be one past the top of the exception stack in Instalog's source
-            insideInstalogCode = true;
-            return;
-        }
-        else if (name == "__tmainCRTStartup")
-        {
-            // __tmainCRTStartup is the first function visible in the CRT; it is the function that calls
-            // main().
-            insideInstalogCode = false;
-            return;
-        }
-        
-        if (insideInstalogCode)
-        {
-            std::cerr << entry.lineFileName << " (" << entry.lineNumber << "): " << entry.undFullName << std::endl;
-        }
-    }
-};
+static int instalog_main();
 
-LONG WINAPI UnhandledExceptionHandler(LPEXCEPTION_POINTERS pointers)
+#ifndef NDEBUG
+int main()
 {
-    if (pointers->ExceptionRecord->ExceptionCode != EH_EXCEPTION_NUMBER)
-    {
-        return EXCEPTION_CONTINUE_SEARCH;
-    }
-
-    std::wcerr << "FAILURE!\n";
-    auto ehExceptionRecord = reinterpret_cast<EHExceptionRecord *>(pointers->ExceptionRecord);
-    auto ehObjectPtr = ehExceptionRecord->params.pExceptionObject;
-    auto stdException = static_cast<std::exception*>(ehObjectPtr);
-    auto hresultExt = dynamic_cast<Instalog::SystemFacades::HresultException*>(stdException);
-    auto win32Ext = dynamic_cast<Instalog::SystemFacades::Win32Exception*>(stdException);
-    if (hresultExt != nullptr)
-    {
-        std::wcerr << L"HRESULT Error: 0x" << std::hex << hresultExt->GetErrorCode() << L": " << hresultExt->GetErrorStringW() << std::endl;
-    }
-    else if (win32Ext != nullptr)
-    {
-        std::wcerr << L"Win32 Error: 0x" << std::hex << win32Ext->GetErrorCode() << L": " << win32Ext->GetWideMessage() << std::endl;
-    }
-    else
-    {
-        std::cerr << "C++ Exception: " << stdException->what() << std::endl;
-    }
-    std::wcerr << "Call Stack:\n";
-    InstalogStackWalker walker;
-    walker.ShowCallstack(::GetCurrentThread(), pointers->ContextRecord);
-    std::cerr << "Press enter to close this window...";
-    std::wcin.get();
-    ::TerminateProcess(::GetCurrentProcess(), 1);
-    return 0;
+	return instalog_main();
 }
+#else
+int main()
+{
+	try
+	{
+		return instalog_main();
+	}
+	catch (Instalog::SystemFacades::HresultException const& hRes)
+	{
+		std::wcerr << "FAILURE!\n";
+        std::wcerr << L"HRESULT Error: 0x" << std::hex << hRes.GetErrorCode() << L": " << hRes.GetErrorStringW() << std::endl;
+	}
+	catch (Instalog::SystemFacades::Win32Exception const& win)
+	{
+		std::wcerr << "FAILURE!\n";
+        std::wcerr << L"Win32 Error: 0x" << std::hex << win.GetErrorCode() << L": " << win.GetWideMessage() << std::endl;
+	}
+	catch (std::exception const& stdExt)
+	{
+        std::cerr << "C++ Exception: " << stdExt.what() << std::endl;
+    }
+
+    return -1;
+}
+#endif
 
 /// @brief    Main entry-point for this application.
-int main()
+static int instalog_main()
 {
     std::wcout <<
         L" ___           _        _\n"
@@ -129,7 +79,6 @@ int main()
         L"|___|_| |_|___/\\__\\__,_|_|\\___/ \\__, |\n"
         L"by Jacob Snyder and Billy ONeal |___/\n";
 
-    ::SetUnhandledExceptionFilter(UnhandledExceptionHandler);
     Instalog::SystemFacades::Com com;
     if (Instalog::SystemFacades::IsWow64())
     {
@@ -154,4 +103,5 @@ int main()
     outFile.close();
     std::wcout << L"Press enter to close this window.";
     std::wcin.get();
+	return 0;
 }
