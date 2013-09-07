b0541d7c9f37947e779c9c90adbdbeeabdd0bd14
Billy O'Neal <billy.oneal@gmail.com>
2013-09-07 01:00:28 -0700

    Updated copyrights.

--- LogTests/DnsTest.cpp
+++ LogTests/DnsTest.cpp
@@ -1,4 +1,4 @@
-// Copyright © 2012 Jacob Snyder, Billy O'Neal III
+// Copyright © 2012-2013 Jacob Snyder, Billy O'Neal III
 // This is under the 2 clause BSD license.
 // See the included LICENSE.TXT file for more details.
 
