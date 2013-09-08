0f02b911d08ec2dbf8c7fa14b62ad609daf66d8e
Billy O'Neal <billy.oneal@gmail.com>
2013-09-07 19:44:15 -0700

    Added a test project to use in file tests with custom version info data.

---
+++ TestProjects/TestVerInfoApp/TestVerInfoApp/main.c
@@ -0,0 +1,10 @@
+#include <windows.h>
+
+void entryPoint()
+{
+    DWORD useless = 0;
+    wchar_t const message[] = L"This is a test program. It exists for its"
+                              L" VS_VERSION_INFO resource.";
+    WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), message,
+                    (sizeof(message) / sizeof(wchar_t)) - 1, &useless, 0);
+}
