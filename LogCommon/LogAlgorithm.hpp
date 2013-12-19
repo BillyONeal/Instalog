86d612ac6ae10eb98e2f027449b43940b119b987
Billy O'Neal <billy.oneal@gmail.com>
2013-12-18 21:51:49 -0800

    Added algorithm "contains".

---
+++ LogCommon/LogAlgorithm.hpp
@@ -0,0 +1,18 @@
+// Copyright © 2013 Billy O'Neal III
+// This is under the 2 clause BSD license.
+// See the included LICENSE.TXT file for more details.
+#pragma once
+#include <algorithm>
+
+namespace Instalog
+{
+    template <typename ForwardIterator, typename T>
+    bool contains(
+        ForwardIterator first,
+        ForwardIterator last,
+        T const& value)
+    {
+        return std::find(first, last, value) != last;
+    }
+
+} // Instalog
---
+++ LogTests/LogAlgorithmTest.cpp
@@ -0,0 +1,20 @@
+// Copyright © 2013 Billy O'Neal III
+// This is under the 2 clause BSD license.
+// See the included LICENSE.TXT file for more details.
+#include "pch.hpp"
+#include "../LogCommon/LogAlgorithm.hpp"
+#include <vector>
+
+using namespace Instalog;
+
+TEST(LogAlgorithm, ContainsTrue)
+{
+    std::vector<int> example{42, 1729, 87};
+    ASSERT_TRUE(contains(example.cbegin(), example.cend(), 1729));
+}
+
+TEST(LogAlgorithm, ContainsFalse)
+{
+    std::vector<int> example{22, 33, 44};
+    ASSERT_FALSE(contains(example.cbegin(), example.cend(), 1729));
+}
