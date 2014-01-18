b9043466f60ad9227f0b555a5e7f9113ec5f76da
Billy O'Neal <billy.oneal@gmail.com>
2014-01-18 15:32:21 -0800

    Removed years from copyrights.

--- LogCommon/ErrorReporter.cpp
+++ LogCommon/ErrorReporter.cpp
@@ -1,4 +1,4 @@
-// Copyright © 2014 Billy O'Neal III
+// Copyright © Billy O'Neal III
 // This is under the 2 clause BSD license.
 // See the included LICENSE.TXT file for more details.
 #include "pch.hpp"
