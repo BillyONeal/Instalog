b0100c895e0080f18a87371d6709e14da1154e23
Billy O'Neal <billy.oneal@gmail.com>
2015-01-21 22:31:59 -0800

    Fixed up CMake to use the static C runtime libraries when building.

---
+++ cxx_flag_overrides.cmake
@@ -0,0 +1,9 @@
+# These settings force use of the static runtime libraries when building C files
+# in cmake.
+if(MSVC)
+    set(CMAKE_CXX_FLAGS_DEBUG_INIT "/D_DEBUG /MTd /Zi /Ob0 /Od /RTC1")
+    set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT     "/MT /O1 /Ob1 /D NDEBUG")
+    set(CMAKE_CXX_FLAGS_RELEASE_INIT        "/MT /O2 /Ob2 /D NDEBUG")
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "/MT /Zi /O2 /Ob1 /D NDEBUG")
+endif()
+
